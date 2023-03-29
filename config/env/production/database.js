e1522e0dc17cd6609e667514317c13688f553df9
gogochang <okpo2581@gmail.com>
2023-03-29 21:40:32 +0900

    install strapi

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,17 @@
     // path: ./config/env/production/database.js
-
     module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
         connection: {
-          client: 'postgres',
-          connection: {
-            host: env('DATABASE_HOST'),
-            port: env.int('DATABASE_PORT'),
-            database: env('DATABASE_NAME'),
-            user: env('DATABASE_USERNAME'),
-            password: env('DATABASE_PASSWORD'),
-            ssl: {
-              ca: env('DATABASE_CA'),
-            },
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
           },
-          debug: false,
         },
-      });
-  
-  
+        debug: false,
+      },
+    });
